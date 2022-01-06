ed6af00f6a25e649cb061c737522aad5d19771e7
Shanmukh <96300319+DShanmukh@users.noreply.github.com>
2022-01-06 15:06:31 +0530

    Add files via upload

---
+++ n.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	
+	int i=1;
+	int n,sum=0;
+	printf("enter n value");
+	scanf("%d",&n);
+	while(i<=n)
+	{
+		product=product*i;
+		i++;
+	}
+	printf("%d",sum);
+	
+}
